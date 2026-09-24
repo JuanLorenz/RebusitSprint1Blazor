a527696db8cf2004e8bdbc36faf2f4cad0eba5f0
LAB23_USER ON WS34 <L23Y16W34@LabHCI.net>
2026-09-24 16:26:13 +0800

    feat: tailwind integration

--- obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bbdac03bbd5968862611d123fb666f6426a12d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07ecdbed05674b9c5950091bfaa2128831abdb1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
